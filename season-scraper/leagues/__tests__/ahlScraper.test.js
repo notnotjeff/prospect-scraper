6fd3bcfba23b373ae553b413e6f7ae0d6ff74791
jefftcraig <jeffcraig35@gmail.com>
2020-11-18 22:44:33 -0500

    Have AHL scraper return null values when season is not there, remove Lindgren

--- season-scraper/leagues/__tests__/ahlScraper.test.js
+++ season-scraper/leagues/__tests__/ahlScraper.test.js
@@ -39,4 +39,27 @@ describe('ahlScraper()', () => {
       await expect(ahlScraper(prospect)).rejects.toThrow()
     })
   })
+
+  describe('when season does not exist', () => {
+    test('it returns null values', async () => {
+      const prospectJson = require('./__fixtures__/ahl_rasmus_sandin.fixture')
+      const prospect = {
+        first_name: 'Rasmus',
+        last_name: 'Sandin',
+        league_id: '7314',
+        league: 'AHL',
+      }
+
+      jest.spyOn(utils.date, 'getCurrentSeason').mockImplementation(() => '2010-11')
+      jest.spyOn(utils.request, 'jsonRequest').mockImplementation(() => prospectJson)
+
+      const { goals, assists, points, shots, games_played } = await ahlScraper(prospect)
+
+      expect(goals).toEqual(null)
+      expect(assists).toEqual(null)
+      expect(points).toEqual(null)
+      expect(shots).toEqual(null)
+      expect(games_played).toEqual(null)
+    })
+  })
 })
