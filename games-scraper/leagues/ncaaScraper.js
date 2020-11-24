0ba8095fb99756686dd3765ba3f882c33d9b9606
jefftcraig <jeffcraig35@gmail.com>
2020-11-24 00:06:00 -0500

    Clean up NCAA scraper to not rely on split, now will iterate through tds and have cleaner game rows

--- games-scraper/leagues/ncaaScraper.js
+++ games-scraper/leagues/ncaaScraper.js
@@ -1,10 +1,19 @@
 const utils = require('../../utils')
 
-// EXAMPLE PROSPECT.JS JSON
-// {
-//   league_id: '16',
-//   league: "NCAA",
-// }
+// Helper function to check table header to make sure we have the right page (should have current season and players name)
+function checkPage(header, firstName, lastName, year) {
+  const sanitizedTableHeader = header.replace(`'`, '').replace(`.`, '')
+  const sanitizedFirstName = firstName.replace(`'`, '').replace(`.`, '')
+  const sanitizedLastName = lastName.replace(`'`, '').replace(`.`, '')
+  const sanitizedYears = year.replace(`'`, '').replace(`.`, '')
+
+  return (
+    !sanitizedTableHeader ||
+    !sanitizedTableHeader.includes(sanitizedLastName) ||
+    !sanitizedTableHeader.includes(sanitizedFirstName) ||
+    !sanitizedTableHeader.includes(sanitizedYears)
+  )
+}
 
 module.exports = async function (prospect, date) {
   if (!prospect.league_id) {
@@ -20,41 +29,31 @@ module.exports = async function (prospect, date) {
   const games = []
   scrapedProspect('body > div.page.text-center > main > section > div > div > div > div.playerstatsfull > table:nth-child(3) > tbody > tr').each(
     function (_i, elm) {
+      const tds = []
       const row = scrapedProspect(elm)
-        .text()
-        .trim()
-        .split('\n')
-        .map(r => r.trim())
-      games.push(row)
+      row.find('td').each(function (_tdI, tdElm) {
+        const td = scrapedProspect(tdElm).text().trim()
+        tds.push(td)
+      })
+      games.push(tds)
     },
   )
 
-  // Check table header to make sure we have the right page (should have current season and players name in the title)
-  const sanitizedTableHeader = String(games?.[0]?.[0]).replace(`'`, '').replace(`.`, '')
-  const sanitizedFirstName = prospect.first_name.replace(`'`, '').replace(`.`, '')
-  const sanitizedLastName = prospect.last_name.replace(`'`, '').replace(`.`, '')
-  const sanitizedYears = currentYear.replace(`'`, '').replace(`.`, '')
+  const isWrongPage = checkPage(String(games?.[0]?.[0]), prospect.first_name, prospect.last_name, currentYear)
 
-  if (
-    !sanitizedTableHeader ||
-    !sanitizedTableHeader.includes(sanitizedLastName) ||
-    !sanitizedTableHeader.includes(sanitizedFirstName) ||
-    !sanitizedTableHeader.includes(sanitizedYears)
-  ) {
+  if (isWrongPage) {
     throw new Error(
       `This is the wrong URL for ${prospect.first_name} ${prospect.last_name}. The NCAA player page changes yearly, make sure you've validated the URL and updated the player's id for the current year.`,
     )
   }
+
   const game = games?.find(g => g[0] === `${month}/${day}/${year}`)
   if (!game) {
     return null
   }
-  const [goals, assists, points] = game[2]
-    .split(' ')[4]
-    .split('-')
-    .map(s => +s)
-  const penalty_minutes = +game[9].split('/')[1].trim()
-  const shots = +game[10]
+  const [goals, assists, points] = game[3].split('-').map(s => +s)
+  const penalty_minutes = +game[10].split('/')[1].trim()
+  const shots = +game[11]
 
   return {
     first_name: prospect.first_name,
