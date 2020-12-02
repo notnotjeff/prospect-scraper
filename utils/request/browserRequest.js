9c044e0d63a18b16238d267257bfdc6d2346ebaa
jefftcraig <jeffcraig35@gmail.com>
2020-12-02 09:12:05 -0500

    Puppeteer launch param fix

--- utils/request/browserRequest.js
+++ utils/request/browserRequest.js
@@ -1,7 +1,9 @@
 const puppeteer = require('puppeteer')
 
 module.exports = async function (url, callback) {
-  const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox'] })
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
   const page = await browser.newPage()
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
   try {
