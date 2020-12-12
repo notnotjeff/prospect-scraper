63b27b2fd1937a569962c2c53086e7297c3def36
jefftcraig <jeffcraig35@gmail.com>
2020-12-12 13:08:00 -0500

    Fix headless in development so hopefully it works on prod *crosses fingers*

--- utils/request/browserRequest.js
+++ utils/request/browserRequest.js
@@ -2,8 +2,8 @@ const puppeteer = require('puppeteer')
 
 module.exports = async function (url, callback) {
   const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--window-size=1920x1080'],
   })
   const page = await browser.newPage()
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
