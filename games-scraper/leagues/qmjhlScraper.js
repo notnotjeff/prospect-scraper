f28026da25f9a60cbe95cb668b1227e8f929d742
jefftcraig <jeffcraig35@gmail.com>
2021-04-26 23:58:18 -0400

    Make qmjhl season id into env variable

--- games-scraper/leagues/qmjhlScraper.js
+++ games-scraper/leagues/qmjhlScraper.js
@@ -1,7 +1,5 @@
 const utils = require('../../utils')
 
-const QMJHL_SEASON_ID = '196'
-
 module.exports = async function (prospect, date) {
   if (!prospect.league_id) {
     throw new Error(`Cannot complete QMJHL scrape, prospect ${prospect.first_name} ${prospect.last_name} is missing: \n league_id`)
@@ -9,7 +7,7 @@ module.exports = async function (prospect, date) {
 
   const { day, month, year } = utils.date.setDateValues(date, { zeroPad: true })
   const monthName = utils.date.getMonthName(date.getMonth(), { isZeroIndexed: true })
-  const url = `https://lscluster.hockeytech.com/feed/index.php?feed=widgetkit2&key=f322673b6bcae299&client_code=lhjmq&view=Gamebygame&lang=en&season_id=${QMJHL_SEASON_ID}&fmt=json&dfdsfdsa=2fdsa&player_id=${prospect.league_id}&force_player=0`
+  const url = `https://lscluster.hockeytech.com/feed/index.php?feed=widgetkit2&key=f322673b6bcae299&client_code=lhjmq&view=Gamebygame&lang=en&season_id=${process.env.QMJHL_SEASON_ID}&fmt=json&dfdsfdsa=2fdsa&player_id=${prospect.league_id}&force_player=0`
   const scrapedProspect = await utils.request.jsonRequest(url)
 
   const game = scrapedProspect?.SiteKit?.Gamebygame?.games[monthName]?.find(({ date_played }) => date_played === `${year}-${month}-${day}`)
