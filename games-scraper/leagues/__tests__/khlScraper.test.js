97c84a0dca6867ee48f02337369c13297599fc8c
Jeff Craig <jeffcraig35@gmail.com>
2022-09-10 18:32:02 -0400

    Update prospect info for start of euro seasons; fix khl scraper with new table classes

--- games-scraper/leagues/__tests__/khlScraper.test.js
+++ games-scraper/leagues/__tests__/khlScraper.test.js
@@ -20,18 +20,18 @@ describe('khlScraper()', () => {
       league: 'KHL',
       ep_url: 'https://www.eliteprospects.com/player/518585/rodion-amirov',
     }
-    const date = new Date('2020-09-26 12:00:00')
+    const date = new Date('2021-12-22 12:00:00')
 
     jest.spyOn(utils.request, 'htmlRequest').mockImplementation(() => cheerio.load(prospectHtml))
 
     const gameData = await khlScraper(prospect, date)
 
-    expect(gameData.goals).toEqual(2)
-    expect(gameData.assists).toEqual(1)
-    expect(gameData.points).toEqual(3)
-    expect(gameData.shots).toEqual(4)
-    expect(gameData.penalty_minutes).toEqual(2)
-    expect(gameData.date).toEqual('2020-09-26')
+    expect(gameData.goals).toEqual(1)
+    expect(gameData.assists).toEqual(0)
+    expect(gameData.points).toEqual(1)
+    expect(gameData.shots).toEqual(2)
+    expect(gameData.penalty_minutes).toEqual(0)
+    expect(gameData.date).toEqual('2021-12-22')
   })
 
   it('it gets prospect html and scrapes for game on date if day of month is single digit', async () => {
@@ -51,18 +51,18 @@ describe('khlScraper()', () => {
       league: 'KHL',
       ep_url: 'https://www.eliteprospects.com/player/518585/rodion-amirov',
     }
-    const date = new Date('2020-09-05 12:00:00')
+    const date = new Date('2021-02-27 12:00:00')
 
     jest.spyOn(utils.request, 'htmlRequest').mockImplementation(() => cheerio.load(prospectHtml))
 
     const gameData = await khlScraper(prospect, date)
 
-    expect(gameData.goals).toEqual(0)
-    expect(gameData.assists).toEqual(1)
+    expect(gameData.goals).toEqual(1)
+    expect(gameData.assists).toEqual(0)
     expect(gameData.points).toEqual(1)
-    expect(gameData.shots).toEqual(2)
+    expect(gameData.shots).toEqual(3)
     expect(gameData.penalty_minutes).toEqual(0)
-    expect(gameData.date).toEqual('2020-09-05')
+    expect(gameData.date).toEqual('2021-02-27')
   })
 
   it('it returns null when there is no game on the specified date', async () => {
