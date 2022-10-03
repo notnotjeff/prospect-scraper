a7317d7807df573b5db21bbfc284b65069ca6fab
Jeff Craig <jeffcraig35@gmail.com>
2022-10-02 22:38:13 -0400

    Use new Liiga website with json

--- season-scraper/leagues/liigaScraper.js
+++ season-scraper/leagues/liigaScraper.js
@@ -5,36 +5,27 @@ module.exports = async function (prospect) {
     throw new Error(`Cannot complete Liiga scrape, prospect ${prospect.first_name} ${prospect.last_name} is missing: \n league_id`)
   }
 
-  const url = `https://old.liiga.fi/en/pelaajat/${prospect.league_id}/${prospect.last_name.toLowerCase()}-${prospect.first_name.toLowerCase()}`
-  const scrapedProspect = await utils.request.htmlRequest(url)
+  const currentSeason = utils.date.getCurrentSeason('YYYY-YYYY').split('-')[1]
+  const url = `https://liiga.fi/api/v1/players/info/${prospect.league_id}`
+  const prospectJson = await utils.request.jsonRequest(url)
+  const seasons = prospectJson.historical.regular.filter(({season}) => season === +currentSeason)
 
-  const currentSeason = utils.date.getCurrentSeason('YYYY-YYYY')
+  if(seasons.length === 0) {
+    return { games_played: undefined, goals: undefined, assists: undefined, points: undefined, shots: undefined }
+  }
 
-  const seasons = []
-  scrapedProspect('#stats-section > table:nth-child(3) > tbody > tr').each(function (_i, elm) {
-    const cells = []
-    scrapedProspect(elm)
-      .find('td')
-      .each(function (_cellI, cellElm) {
-        const cell = scrapedProspect(cellElm).text().trim().replace('\n', '').replace('\t', '')
-        cells.push(cell)
-      })
-    seasons.push(cells)
-  })
+  const { games_played, goals, assists, points, shots } = seasons.reduce(
+    (acc, team) => {
+      return {
+        goals: acc.goals + team.goals,
+        assists: acc.assists + team.assists,
+        points: acc.points + team.points,
+        shots: acc.shots + team.shots,
+        games_played: acc.games_played + team.games,
+      }
+    },
+    { goals: 0, assists: 0, points: 0, shots: 0, games_played: 0 },
+  )
 
-  const season = seasons
-    ?.filter(s => s[0] === currentSeason)
-    ?.reduce(
-      (previousSeason, currentSeason) => {
-        previousSeason.goals += +currentSeason[4]
-        previousSeason.assists += +currentSeason[5]
-        previousSeason.points += +currentSeason[6]
-        previousSeason.shots += +currentSeason[14]
-        previousSeason.games_played += +currentSeason[3]
-        return previousSeason
-      },
-      { goals: null, assists: null, points: null, shots: null, games_played: null },
-    )
-
-  return season
+  return { games_played, goals, assists, points, shots }
 }
